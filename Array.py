a9106d06fd499f4ec4d09e522eabefb0db29d355
ahmedrashadtxt <ahmed.rashadtxt@gmail.com>
2021-12-13 19:54:41 +0530

    removed files

--- Array.py
+++
@@ -1,4 +0,0 @@
-import ctypes
-
-ArrayType = ctypes.py_object * 5
-slots = ArrayType()
--- palindrome.py
+++
@@ -1,6 +0,0 @@
-string = input("Enter string: ").lower()
-
-if (string==string[::-1]):
-    print("Palindrome")
-else:
-    print("Nah")
